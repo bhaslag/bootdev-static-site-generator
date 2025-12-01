2f3b5af212e25afb7085223b3c146ca7bdc3f8fd
wihas <wihas@smile.fr>
2025-12-01 13:41:58 +0100

    Added leaf node, parent node, text to html function

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -11,7 +11,9 @@ class HTMLNode:
   def props_to_html(self):
     if self.props is None:
       return ""
-    props_html = " " + " ".join([f"{key}={value}" for key, value in self.props.items()])
+
+    props_html = " " + " ".join([f'{key}="{value}"' for key, value in self.props.items()])
+    
     return props_html
   
   def __repr__(self):
@@ -19,5 +21,31 @@ class HTMLNode:
       TAG = self.tag,
       VALUE = self.value,
       CHILDREN = self.children,
-      PROPS = self.props_to_html()
+      PROPS = self.props
     )
+  
+class LeafNode(HTMLNode):
+  def __init__(self, tag, value, props=None):
+    super().__init__(tag, value, None, props)
+
+  def to_html(self):
+    if self.value is None:
+      raise ValueError("LeafNode must have a value")
+
+    if (self.tag is None):
+      return self.value
+
+    return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+  
+class ParentNode(HTMLNode):
+  def __init__(self, tag, children, props=None):
+    super().__init__(tag, None, children, props)
+
+  def to_html(self):
+    if self.tag is None:
+      raise ValueError("ParentNode must have a tag")
+    
+    if self.children == []:
+      raise ValueError("Children cannot be empty")
+    
+    return f"<{self.tag}{self.props_to_html()}>{''.join(child.to_html() for child in self.children)}</{self.tag}>"
--- src/textnode.py
+++ src/textnode.py
@@ -1,6 +1,7 @@
 from enum import Enum
+from htmlnode import LeafNode
 
-TextType = Enum('TextType', [('PLAIN', 'plain'), ('BOLD','bold'), ('ITALIC', 'italic'), ('CODE', 'code'), ('LINK', 'link'), ('IMAGE', 'image')])
+TextType = Enum('TextType', [('TEXT', 'text'), ('BOLD','bold'), ('ITALIC', 'italic'), ('CODE', 'code'), ('LINK', 'link'), ('IMAGE', 'image')])
 
 class TextNode:
     def __init__(self, text, text_type, url=None):
@@ -17,3 +18,26 @@ class TextNode:
             TEXT_TYPE = self.text_type.value,
             URL = self.url
         )
+    
+def text_node_to_html_node(text_node):
+  match text_node.text_type:
+      case TextType.TEXT:
+          return LeafNode(None, text_node.text)
+      
+      case TextType.BOLD:
+          return LeafNode( "b", text_node.text)
+      
+      case TextType.ITALIC:
+          return LeafNode("i", text_node.text)
+      
+      case TextType.CODE:
+          return LeafNode("code", text_node.text)
+      
+      case TextType.LINK:
+          return LeafNode( "a", text_node.text, {"href": text_node.url})
+      
+      case TextType.IMAGE:
+          return LeafNode("img", '', {"src": text_node.url, "alt": text_node.text})
+      
+      case _:
+        raise ValueError(f"invalid text type: {text_node.text_type}")
