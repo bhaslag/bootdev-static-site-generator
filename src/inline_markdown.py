5f17489435bf95745a9e6046b5bc16b9b31922e2
wihas <wihas@smile.fr>
2025-12-10 14:35:52 +0100

    added more tests, refactored splitting functions

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -33,44 +33,74 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
     return new_nodes
 
 def split_nodes_link(old_nodes):
-  new_nodes = []
+    new_nodes = []
 
-  for node in old_nodes:
-    current_text = node.text
+    for old_node in old_nodes:
+        if old_node.text_type != TextType.TEXT:
+            new_nodes.append(old_node)
+            continue
 
-    for link_text, link_url in extract_markdown_links(node.text):
-      sections = current_text.split(f"[{link_text}]({link_url})", 1)
-      before = sections[0]
-      after = sections[1]
+        original_text = old_node.text
+        links = extract_markdown_links(original_text)
 
-      if (len(before) > 0):
-        new_nodes.append(TextNode(before, TextType.TEXT))
+        if len(links) == 0:
+            new_nodes.append(old_node)
+            continue
 
-      new_nodes.append(TextNode(link_text, TextType.LINK, link_url))
+        for link in links:
+            sections = original_text.split(f"[{link[0]}]({link[1]})", 1)
 
-      current_text = after
+            if len(sections) != 2:
+                raise ValueError("invalid markdown, link section not closed")
 
-  return new_nodes
+            if sections[0] != "":
+                new_nodes.append(TextNode(sections[0], TextType.TEXT))
+
+            new_nodes.append(TextNode(link[0], TextType.LINK, link[1]))
+            original_text = sections[1]
+
+        if original_text != "":
+            new_nodes.append(TextNode(original_text, TextType.TEXT))
+
+    return new_nodes
 
 def split_nodes_image(old_nodes):
-  new_nodes = []
+    new_nodes = []
 
-  for node in old_nodes:
-    current_text = node.text
+    for old_node in old_nodes:
+        if old_node.text_type != TextType.TEXT:
+            new_nodes.append(old_node)
+            continue
 
-    for img_alt, img_src in extract_markdown_images(node.text):
-      sections = current_text.split(f"![{img_alt}]({img_src})", 1)
-      before = sections[0]
-      after = sections[1]
+        original_text = old_node.text
+        images = extract_markdown_images(original_text)
 
-      if (len(before) > 0):
-        new_nodes.append(TextNode(before, TextType.TEXT))
+        if len(images) == 0:
+            new_nodes.append(old_node)
+            continue
 
-      new_nodes.append(TextNode(img_alt, TextType.IMAGE, img_src))
+        for image in images:
+            sections = original_text.split(f"![{image[0]}]({image[1]})", 1)
 
-      current_text = after
+            if len(sections) != 2:
+                raise ValueError("invalid markdown, image section not closed")
 
-  return new_nodes
+            if sections[0] != "":
+                new_nodes.append(TextNode(sections[0], TextType.TEXT))
+
+            new_nodes.append(
+                TextNode(
+                    image[0],
+                    TextType.IMAGE,
+                    image[1],
+                )
+            )
+            original_text = sections[1]
+
+        if original_text != "":
+            new_nodes.append(TextNode(original_text, TextType.TEXT))
+
+    return new_nodes
 
 def extract_markdown_images(text):
   pattern = r"!\[([^\[\]]*)\]\(([^\(\)]*)\)"
