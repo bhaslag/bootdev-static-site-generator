eb82504637754317b6335917956ab9984f48574a
wihas <wihas@smile.fr>
2025-12-10 14:13:33 +0100

    finished split nodes by image and link

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -36,11 +36,41 @@ def split_nodes_link(old_nodes):
   new_nodes = []
 
   for node in old_nodes:
-    links = extract_markdown_links(node.text)
-  # print(links)
-  for link_text, link_url in links:
-    sections = node.text.split(f"![{link_text}]({link_url})")
-  print(sections)
+    current_text = node.text
+
+    for link_text, link_url in extract_markdown_links(node.text):
+      sections = current_text.split(f"[{link_text}]({link_url})", 1)
+      before = sections[0]
+      after = sections[1]
+
+      if (len(before) > 0):
+        new_nodes.append(TextNode(before, TextType.TEXT))
+
+      new_nodes.append(TextNode(link_text, TextType.LINK, link_url))
+
+      current_text = after
+
+  return new_nodes
+
+def split_nodes_image(old_nodes):
+  new_nodes = []
+
+  for node in old_nodes:
+    current_text = node.text
+
+    for img_alt, img_src in extract_markdown_images(node.text):
+      sections = current_text.split(f"![{img_alt}]({img_src})", 1)
+      before = sections[0]
+      after = sections[1]
+
+      if (len(before) > 0):
+        new_nodes.append(TextNode(before, TextType.TEXT))
+
+      new_nodes.append(TextNode(img_alt, TextType.IMAGE, img_src))
+
+      current_text = after
+
+  return new_nodes
 
 def extract_markdown_images(text):
   pattern = r"!\[([^\[\]]*)\]\(([^\(\)]*)\)"
--- src/test_inline_markdown.py
+++ src/test_inline_markdown.py
@@ -2,6 +2,7 @@ import unittest
 from inline_markdown import (
     split_nodes_delimiter,
     split_nodes_link,
+    split_nodes_image,
     extract_markdown_images,
     extract_markdown_links,
 )
@@ -118,6 +119,24 @@ class TestInlineMarkdown(unittest.TestCase):
               ),
           ],
         new_nodes,
+      )
+
+    def test_split_images(self):
+      node = TextNode(
+          "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) and another ![second image](https://i.imgur.com/3elNhQu.png)",
+          TextType.TEXT,
+      )
+      new_nodes = split_nodes_image([node])
+      self.assertListEqual(
+          [
+              TextNode("This is text with an ", TextType.TEXT),
+              TextNode("image", TextType.IMAGE, "https://i.imgur.com/zjjcJKZ.png"),
+              TextNode(" and another ", TextType.TEXT),
+              TextNode(
+                  "second image", TextType.IMAGE, "https://i.imgur.com/3elNhQu.png"
+              ),
+          ],
+          new_nodes,
       )
 
 if __name__ == "__main__":
